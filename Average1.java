5ca1e4db8d4f9ef7b500362eed9b474460148268
Милашевський Олександр <milan7667@gmail.com>
2019-12-01 23:40:23 +0200

    Tsk 5 fixing double

--- Average1.java
+++ Average1.java
@@ -2,10 +2,10 @@ package CursorEducation;
 
 public class Average1 {
     public static void main(String[] args) {
-        int[] myArr = {6, 42, 3, 7};
+        double [] myArr = {6, 42, 3, 7};
         double sumAverage = 0;
         for (int x = 0; x < myArr.length; x++) {
-            sumAverage += myArr[x] / myArr.length;
+            sumAverage = sumAverage + myArr[x] / myArr.length;
         }
         System.out.println(sumAverage);
     }
