198bba70babd88280032358c0c7df3bac22a21b7
Милашевський Олександр <milan7667@gmail.com>
2019-12-05 20:58:33 +0200

    Homework 3 after fixing2

--- Circle.java
+++ Circle.java
@@ -3,15 +3,15 @@ package CursorEducation;
 import static java.lang.Math.pow;
 
 public class Circle {
-    public static double getArea(double radius) {
+    public static double calculateArea(double radius) {
         return Math.PI * pow(radius, 2);
 
     }
 
     public static void main(String[] args) {
         double radius = 66.77;
-        double getArea = getArea(radius);
+        double calculateArea = calculateArea(radius);
 
-        System.out.println(getArea);
+        System.out.println(calculateArea);
     }
 }
